0804e8a821de6524f3855a17e28c2a765b3bb256
zhangwangqiang <zhangwangqiang@airm2m.com>
2021-04-14 14:10:18 +0800

    index

--- fiber/fiber-react/src/index.js
+++ fiber/fiber-react/src/index.js
@@ -1,38 +1,48 @@
 import React from './react';
-// import ReactDOM from './ReactDom/index';
+import ReactDOM from './ReactDom/index';
 
-let style = { border: '3px solid red', margin: '5px' }
-// JSX
-let element = (
-  <div id="A1" style={style}>
-    A1
-    <div id="B1" style={style}>
-      B1
-      <div id="C1" style={style}>C1</div>
-      <div id="C2" style={style}>C2</div>
-    </div>
-    <div id="B2" style={style}>B2</div>
-  </div>
-)
+// let style = { border: '3px solid red', margin: '5px' }
+// // JSX
+// let element = (
+//   <div id="A1" style={style}>
+//     A1
+//     <div id="B1" style={style}>
+//       B1
+//       <div id="C1" style={style}>C1</div>
+//       <div id="C2" style={style}>C2</div>
+//     </div>
+//     <div id="B2" style={style}>B2</div>
+//   </div>
+// )
 
-// let element = React.createElement("div", {
-//   id: "A1"
-// }, React.createElement("div", {
-//   id: "B1"
-// }, React.createElement("div", {
-//   id: "C1"
-// }), React.createElement("div", {
-//   id: "C2"
-// })), React.createElement("div", {
-//   id: "B2"
-// }));
+let style = {
+  border: '3px solid red',
+  margin: '5px'
+}; // JSX
+
+let element = /*#__PURE__*/React.createElement("div", {
+  id: "A1",
+  style: style
+}, "A1", /*#__PURE__*/React.createElement("div", {
+  id: "B1",
+  style: style
+}, "B1", /*#__PURE__*/React.createElement("div", {
+  id: "C1",
+  style: style
+}, "C1"), /*#__PURE__*/React.createElement("div", {
+  id: "C2",
+  style: style
+}, "C2")), /*#__PURE__*/React.createElement("div", {
+  id: "B2",
+  style: style
+}, "B2"));
 
 console.log(element);
 
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 
 // ReactDOM.render(
 //   <React.StrictMode>
